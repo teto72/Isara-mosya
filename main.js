69f6e1e812d70cf91dc9951e45ca502fbb90d427
teto72 <tsubasa.k.729@gmail.com>
2020-08-17 22:36:35 +0900

    add:questions
    よくある質問部分の追加（計15Q）

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-$(function () {
-  $('button').click(function () {
-      $('p').toggle();
+$(function(){
+  $('.section').hide();
+
+  $('.list').on('click',function(){
+    $('.section').not($($(this).attr('href'))).hide();
+
+    // フェードイン・アウトのアニメーション付で、表示・非表示を交互に実行する
+    $($(this).attr('href')).fadeToggle(10);
+
+    // show を使うと、表示するだけ （ 同じボタンを何回押しても変わらない ）
+    // $($(this).attr('href')).show();
   });
 });
